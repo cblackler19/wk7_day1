17c91a1d0c61958019eab196c94ca3f1d46d5739
Caleb Blackler <cblackler19@gmail.com>
2021-03-22 16:24:11 +1300

    UPDATE: Added HDD size

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-const __smallNum=16;
-let bigNumber=0;
+const __smallNum=1000; // 8 = 8192 if isHDD == FALSE. 8 = 8000 if isHDD == TRUE.
+const isHDD=true // BOOLEAN: TRUE or FALSE.
+let bigNumber=0; // __smallNum in its big byte form.
 
-const realNumbers=()=>{
-    bigNumber=__smallNum*1024;
-    console.log(bigNumber);
-};realNumbers();
+const realNumbers=(a)=>{
+    if(isHDD){
+        let bytes = __smallNum*1000000000;
+        bigNumber=bytes/(1024*1024*1024);
+        console.log('HDD SIZE: ',bigNumber.toFixed(2),'GB');
+    }else{
+        bigNumber=a*1024;
+        console.log('MEMORY SIZE: ',bigNumber,'MB')
+    }
+};realNumbers(__smallNum);
